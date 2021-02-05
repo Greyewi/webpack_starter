3e6e92dbdb51e03b9054ca504c9190f15064cfce
greyewi1 <greyewi@gmail.com>
2021-02-05 13:33:40 +0200

    add template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Как интересно!")
+const heading = document.createElement('h1')
+heading.textContent = 'Hello world!'
+
+const root = document.querySelector('#root')
+root.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
@@ -8,4 +10,11 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack template',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+  ],
 }
